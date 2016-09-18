6c74429231d8dd88029650042aea48a4b07d2b25
Mayra Moreira <mayrita-ms@hotmail.com>
2016-09-18 15:54:39 -0500

    Preuba de Conexion de BD

--- src/dao/CorporativoDAOImpl.java
+++ src/dao/CorporativoDAOImpl.java
@@ -1,5 +1,44 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import conexion.Conexion;
+import dtos.PersonaDTO;
+
 public class CorporativoDAOImpl implements CorporativoDAO {
+	private Connection userConn;
+	
+	
+	
+	public PersonaDTO obtenerPersonaPorId(Long id) throws SQLException{
+		Connection conn=null;
+		Statement stmt=null;
+		ResultSet datos=null;
+		PersonaDTO persona=new PersonaDTO();
+		try{
+			conn=(this.userConn!=null)?this.userConn:Conexion.getConnection();
+			stmt=conn.createStatement();
+            datos=stmt.executeQuery("SELECT * FROM persona WHERE ID=1");
+            while(datos.next()){
+            	persona.setId(datos.getLong(1));
+            	persona.setNombre(datos.getString(2));
+            	persona.setEstado(datos.getString(3));
+            }
+		}
+		finally{
+			Conexion.close(stmt);
+			if(this.userConn==null){
+				Conexion.close(conn);
+			}
+		} 
+        return persona;
+	}
 
 }
+
+
+
+
--- src/run/Run.java
+++ src/run/Run.java
@@ -1,10 +1,22 @@
 package run;
 
+import java.sql.SQLException;
+
+import dao.CorporativoDAO;
+import dao.CorporativoDAOImpl;
+import dtos.PersonaDTO;
+
 public class Run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		CorporativoDAO corporativo= new CorporativoDAOImpl();
+		try {
+			PersonaDTO p=corporativo.obtenerPersonaPorId(1L);
+			System.out.println(p.getId()+ " "+ p.getNombre());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	
 	}
 
 }
