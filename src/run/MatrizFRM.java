5f431339f9724c6c90c0aad912cf3927730b4ec0
Mayra Moreira <mayrita-ms@hotmail.com>
2016-09-18 23:13:28 -0500

    matriz  de puestos

---
+++ src/run/MatrizFRM.java
@@ -0,0 +1,19 @@
+package run;
+
+import javax.swing.JPanel;
+
+public class MatrizFRM extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6312166505526175828L;
+
+	public MatrizFRM (){
+		initComponent();
+	}
+
+	private void initComponent() {
+		
+	}
+}
